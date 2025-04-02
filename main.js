3539ee111bc5c77123cb204484f37f113fae5cd2
Arad190783 <noar24xe@student.ju.se>
2025-04-02 14:55:37 +0200

    yo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let users = [];
 let posts = [];
+const contentElement = document.getElementById("content");
 
 async function loadData() {
   const userResponse = await fetch("data/users.json");
@@ -10,6 +11,29 @@ async function loadData() {
   const postResponse = await fetch("data/posts.json");
   const postJSON = await postResponse.json();
   posts = postJSON.posts;
+
+  renderContent();
+}
+
+function createPostElement(post, user) {
+  const postElement = document.createElement("article");
+  postElement.classList.add("post");
+
+  const imageElement = document.createElement("img");
+  imageElement.src = post.image;
+  postElement.appendChild(imageElement);
+
+  return postElement;
+}
+
+function renderContent() {
+  contentElement.innerHTML = "";
+
+  for (let post of posts) {
+    const user = getUserByID(post.user_id);
+    const postElement = createPostElement(post, user);
+    contentElement.appendChild(postElement);
+  }
 }
 
 loadData();
